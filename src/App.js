54857da6563e61a96bbafe0f5b38a491199e1422
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-06-23 19:50:18 -0500

    add message to html page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website currently under construction
+          Website Currently Under Construction
         </p>
       </header>
     </div>
